40e22c886deaaaaabf0c01bb03df5e7d8e5f90d5
HP <akmalxojanazarov312@gmail.com>
2024-11-09 18:09:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialization for ES Users
+// import { Ripple, initMDB } from "mdb-ui-kit";
+
+// initMDB({ Ripple });
+
+const mms=document.querySelector("#MMS")
+
+mms.addEventListener("select",()=>{
+    console.log('sarvar');
+})
